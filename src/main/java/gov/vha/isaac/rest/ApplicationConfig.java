d3e27f2d88588885d392c4548c595f868e3a3ded
darmbrust <daniel.armbrust.list@gmail.com>
2016-02-09 23:59:13 -0500

    ensure config doesn't run more than once

--- src/main/java/gov/vha/isaac/rest/ApplicationConfig.java
+++ src/main/java/gov/vha/isaac/rest/ApplicationConfig.java
@@ -1,14 +1,11 @@
 package gov.vha.isaac.rest;
 
 import java.io.File;
-import javax.annotation.PostConstruct;
+import java.util.concurrent.atomic.AtomicInteger;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-import org.glassfish.hk2.api.MultiException;
-import org.glassfish.hk2.api.ServiceLocator;
-import org.glassfish.hk2.runlevel.RunLevelController;
-import org.glassfish.hk2.runlevel.internal.AsyncRunLevelContext;
-import org.glassfish.hk2.utilities.ServiceLocatorUtilities;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.glassfish.jersey.server.spi.Container;
 import org.glassfish.jersey.server.spi.ContainerLifecycleListener;
 import gov.vha.isaac.MetaData;
@@ -27,6 +24,54 @@ import gov.vha.isaac.ochre.model.configuration.StampCoordinates;
 @ApplicationPath("rest/")
 public class ApplicationConfig extends Application implements ContainerLifecycleListener
 {
+	private static final AtomicInteger startup = new AtomicInteger(1);
+	private static Logger log = LogManager.getLogger();
+	
+	public static void issacInit()
+	{
+		log.info("Rest Configuration Requested");
+		if (startup.getAndDecrement() == 1)
+		{
+			log.info("Executing ISAAC Init");
+			//do startup in this thread
+			LookupService.get();
+			
+	
+			//TODO background thread this
+			//File dataStoreLocation = DBLocator.findDBFolder(new File("E:/EclipseWorkspaces/ISAAC2/ISAAC-fx-gui-pa/fx-gui-assembly/vhat-2016.01.07-1.0-SNAPSHOT-all.data"));
+			//File dataStoreLocation = DBLocator.findDBFolder(new File("E:/EclipseWorkspaces/ISAAC2/ISAAC-fx-gui-pa/fx-gui-assembly/snomed-20150731-1.0-SNAPSHOT-all.data"));
+			File dataStoreLocation = DBLocator.findDBFolder(new File("/opt/weblogic/vhat-2016.01.07-1.0-SNAPSHOT-all.data"));
+	
+			if (!dataStoreLocation.exists())
+			{
+				throw new RuntimeException("Couldn't find a data store from the input of '" + dataStoreLocation.getAbsoluteFile().getAbsolutePath() + "'");
+			}
+			if (!dataStoreLocation.isDirectory())
+			{
+				throw new RuntimeException("The specified data store: '" + dataStoreLocation.getAbsolutePath() + "' is not a folder");
+			}
+	
+			LookupService.getService(ConfigurationService.class).setDataStoreFolderPath(dataStoreLocation.toPath());
+			System.out.println("  Setup AppContext, data store location = " + dataStoreLocation.getAbsolutePath());
+	
+			LookupService.startupIsaac();
+	
+			System.out.println("Done setting up ISAAC");
+	
+			System.out.println("System up...");
+	
+			StampCoordinate stampCoordinate = StampCoordinates.getDevelopmentLatest();
+			LanguageCoordinate languageCoordinate = LanguageCoordinates.getUsEnglishLanguageFullySpecifiedNameCoordinate();
+	
+			ConceptService conceptService = Get.conceptService();
+	
+			ConceptChronology<? extends ConceptVersion<?>> sctId = conceptService.getConcept(MetaData.SNOMED_INTEGER_ID.getPrimordialUuid());
+			System.out.println("Found [1]: " + sctId);
+	
+			System.out.println(conceptService.getSnapshot(stampCoordinate, languageCoordinate).getConceptSnapshot(sctId.getNid()));
+		}
+	}
+	
 	@Override
 	public void onReload(Container arg0)
 	{
@@ -42,7 +87,7 @@ public class ApplicationConfig extends Application implements ContainerLifecycle
 	@Override
 	public void onStartup(Container container)
 	{
-		
+		log.info("onStartup called");
 		//Bunch of junk debug code from hacking at glassfish...
 		//ServiceLocator jerseyServiceLocator = container.getApplicationHandler().getServiceLocator();
 		
@@ -69,37 +114,6 @@ public class ApplicationConfig extends Application implements ContainerLifecycle
 		//Make sure the service Locator comes up ok
 		//LookupService.setExistingLocator(null);
 		
-		LookupService.get();
-
-		//TODO background thread this
-		File dataStoreLocation = DBLocator.findDBFolder(new File("E:/EclipseWorkspaces/ISAAC2/ISAAC-fx-gui-pa/fx-gui-assembly/vhat-2016.01.07-1.0-SNAPSHOT-all.data"));
-
-		if (!dataStoreLocation.exists())
-		{
-			throw new RuntimeException("Couldn't find a data store from the input of '" + dataStoreLocation.getAbsoluteFile().getAbsolutePath() + "'");
-		}
-		if (!dataStoreLocation.isDirectory())
-		{
-			throw new RuntimeException("The specified data store: '" + dataStoreLocation.getAbsolutePath() + "' is not a folder");
-		}
-
-		LookupService.getService(ConfigurationService.class).setDataStoreFolderPath(dataStoreLocation.toPath());
-		System.out.println("  Setup AppContext, data store location = " + dataStoreLocation.getAbsolutePath());
-
-		LookupService.startupIsaac();
-
-		System.out.println("Done setting up ISAAC");
-
-		System.out.println("System up...");
-
-		StampCoordinate stampCoordinate = StampCoordinates.getDevelopmentLatest();
-		LanguageCoordinate languageCoordinate = LanguageCoordinates.getUsEnglishLanguageFullySpecifiedNameCoordinate();
-
-		ConceptService conceptService = Get.conceptService();
-
-		ConceptChronology<? extends ConceptVersion<?>> sctId = conceptService.getConcept(MetaData.SNOMED_INTEGER_ID.getPrimordialUuid());
-		System.out.println("Found [1]: " + sctId);
-
-		System.out.println(conceptService.getSnapshot(stampCoordinate, languageCoordinate).getConceptSnapshot(sctId.getNid()));
+		issacInit();
 	}
 }
