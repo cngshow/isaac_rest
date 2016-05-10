80840845b4f78f53a66e8b7ac39da9caf9c73b02
darmbrust <daniel.armbrust.list@gmail.com>
2016-05-10 16:58:18 -0400

    point at newly released vets DB by default for dev fallback

--- src/main/java/gov/vha/isaac/rest/ApplicationConfig.java
+++ src/main/java/gov/vha/isaac/rest/ApplicationConfig.java
@@ -310,7 +310,7 @@ public class ApplicationConfig extends ResourceConfig implements ContainerLifecy
 				mavenPassword = "system";
 				groupId = "gov.vha.isaac.db";
 				artifactId = "vets";
-				version = "1.0";
+				version = "1.1";
 				classifier = "all";
 			}
 			
