c24e5743dd17985e5c46d5fae8173613c8d53d59
darmbrust <daniel.armbrust.list@gmail.com>
2016-05-18 11:38:12 -0500

    clean up system info, remove fields that didn't make sense, validate that version from prisme.properties matches version from pom.xml

--- src/main/java/gov/vha/isaac/rest/api1/data/SystemInfo.java
+++ src/main/java/gov/vha/isaac/rest/api1/data/SystemInfo.java
@@ -18,15 +18,38 @@
  */
 package gov.vha.isaac.rest.api1.data;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
+import java.util.Properties;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
-
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.w3c.dom.DOMException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
-
+import gov.vha.isaac.ochre.api.ConfigurationService;
+import gov.vha.isaac.ochre.api.LookupService;
 import gov.vha.isaac.rest.api1.data.systeminfo.RestDependencyInfo;
 import gov.vha.isaac.rest.api1.data.systeminfo.RestLicenseInfo;
 
@@ -44,6 +67,7 @@ import gov.vha.isaac.rest.api1.data.systeminfo.RestLicenseInfo;
 @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY)
 public class SystemInfo
 {
+	private transient static Logger log_ = LogManager.getLogger();
 	/**
 	 * The full version number of this API.  Note, this is an array, because in the future
 	 * the API may simultaneously support versions such as [1.3, 2.0] for reverse compatibility.
@@ -61,47 +85,29 @@ public class SystemInfo
 	String[] supportedAPIVersions = new String[] {"1.3.3"};
 	
 	/**
-	 * REST API Implementation Version
+	 * REST API Implementation Version - aka the version number of the software running here.
 	 */
 	@XmlElement
 	public String apiImplementationVersion;
 	
 	/**
-	 * ISAAC DB Maven dependency
+	 * The version number of the database being used by this instance.
 	 */
 	@XmlElement
 	public RestDependencyInfo isaacDbDependency;
 
 	/**
-	 * Source Code Management URL
+	 * Source Code Management URL that contains the source code for the software running here.
 	 */
 	@XmlElement
 	public String scmUrl;
 	
 	/**
-	 * ISAAC Version
+	 * The version of ISAAC that the rest service is running on top of.
 	 */
 	@XmlElement
 	public String isaacVersion;
 	
-	/**
-	 * ISAAC GUI Version
-	 */
-	@XmlElement
-	public String isaacGuiVersion;
-	
-	/**
-	 * ISAAC Assembly Version
-	 */
-	@XmlElement
-	public String assemblyVersion;
-	
-	/**
-	 * Metadata Version
-	 */
-	@XmlElement
-	public String metadataVersion = "?";
-
 	/**
 	 * Software Licenses
 	 */
@@ -115,100 +121,23 @@ public class SystemInfo
 	List<RestLicenseInfo> dbLicenses = new ArrayList<>();
 	
 	/**
-	 * Additional Database Maven Dependencies
+	 * The source content that was built into the underlying database.
 	 */
 	@XmlElement
 	List<RestDependencyInfo> dbDependencies = new ArrayList<>();
 
 	public SystemInfo()
 	{
-		//For jaxb
-	}
-
-	/**
-	 * @param supportedAPIVersions the supportedAPIVersions to set
-	 * 
-	 * TODO Set supportedAPIVersions from available packages, directories, files or file contents
-	 */
-	public void setSupportedAPIVersions(String[] supportedAPIVersions) {
-		this.supportedAPIVersions = supportedAPIVersions;
-	}
-
-	/**
-	 * @param ISAAC DB Maven dependency to set
-	 */
-	public void setIsaacDbDependency(RestDependencyInfo dbDependency) {
-		this.isaacDbDependency = dbDependency;
-	}
-
-	/**
-	 * @param scmUrl the scmUrl to set
-	 */
-	public void setScmUrl(String scmUrl) {
-		this.scmUrl = scmUrl;
-	}
-
-	/**
-	 * @param isaacVersion the isaacVersion to set
-	 */
-	public void setIsaacVersion(String isaacVersion) {
-		this.isaacVersion = isaacVersion;
-	}
-
-	/**
-	 * @param isaacGuiVersion the isaacGuiVersion to set
-	 */
-	public void setIsaacGuiVersion(String isaacGuiVersion) {
-		this.isaacGuiVersion = isaacGuiVersion;
-	}
-
-	/**
-	 * @param assemblyVersion the assemblyVersion to set
-	 */
-	public void setAssemblyVersion(String assemblyVersion) {
-		this.assemblyVersion = assemblyVersion;
-	}
-
-	/**
-	 * @param metadataVersion the metadataVersion to set
-	 */
-	public String setMetadataVersion(String metadataVersion) {
-		return this.metadataVersion = metadataVersion;
-	}
-
-	/**
-	 * @return the dbDependencies
-	 */
-	public List<RestDependencyInfo> getDbDependencies() {
-		return dbDependencies;
-	}
-
-	/**
-	 * @param dbDependencies the dbDependencies to set
-	 */
-	public void addDbDependency(RestDependencyInfo dbDependency) {
-		this.dbDependencies.add(dbDependency);
-	}
-
-	/**
-	 * @return the dbLicenses
-	 */
-	public List<RestLicenseInfo> getDbLicenses() {
-		return dbLicenses;
-	}
-
-	/**
-	 * @param dbLicenses the dbLicenses to set
-	 */
-	public void addDbLicense(RestLicenseInfo dbLicense) {
-		this.dbLicenses.add(dbLicense);
-	}
-
-	/**
-	 * @param appLicenses the appLicenses to set
-	 */
-	public void addAppLicense(RestLicenseInfo appLicense) {
-		this.appLicenses.add(appLicense);
+		//Read in other information from the package (pom.properties file during normal runtime, pom.xml files if running in a dev env)
+		try
+		{
+			loadIsaacMetdata();
+			loadDbMetadata();
+		}
+		catch (Exception ex)
+		{
+			log_.error("Unexpected error reading app configuration information", ex);
+		}
 	}
 
 	/* (non-Javadoc)
@@ -216,10 +145,222 @@ public class SystemInfo
 	 */
 	@Override
 	public String toString() {
-		return "SystemInfo [supportedAPIVersions=" + Arrays.toString(supportedAPIVersions) + ", restVersion="
-				+ apiImplementationVersion + ", isaacDbDependency=" + isaacDbDependency + ", scmUrl=" + scmUrl + ", isaacVersion="
-				+ isaacVersion + ", isaacGuiVersion=" + isaacGuiVersion + ", assemblyVersion=" + assemblyVersion
-				+ ", metadataVersion=" + metadataVersion + ", appLicenses=" + appLicenses + ", dbLicenses=" + dbLicenses
+		return "SystemInfo [supportedAPIVersions=" + Arrays.toString(supportedAPIVersions) + ", apiImplementationVersion="
+				+ apiImplementationVersion + ", isaacDbDependency=" + isaacDbDependency + ", scmUrl=" + scmUrl 
+				+ ", isaacVersion="
+				+ isaacVersion
+				+ ", appLicenses=" + appLicenses + ", dbLicenses=" + dbLicenses
 				+ ", dbDependencies=" + dbDependencies + "]";
+	}
+	
+	// Read the DB metadata
+	private void loadDbMetadata() throws IOException {
+		//Read the db metadata
+		AtomicBoolean readDbMetadataFromProperties = new AtomicBoolean(false);
+		AtomicBoolean readDbMetadataFromPom = new AtomicBoolean(false);
+		
+		java.nio.file.Path dbLocation = LookupService.get().getService(ConfigurationService.class).getChronicleFolderPath().getParent();
+		//find the pom.properties file in the hierarchy
+		File dbMetadata = new File(dbLocation.toFile(), "META-INF");
+		AtomicReference<String> metadataVersion = new AtomicReference<String>("");
+		if (dbMetadata.isDirectory())
+		{
+			Files.walkFileTree(dbMetadata.toPath(), new SimpleFileVisitor<java.nio.file.Path>()
+			{
+				/**
+				 * @see java.nio.file.SimpleFileVisitor#visitFile(java.lang.Object, java.nio.file.attribute.BasicFileAttributes)
+				 */
+				@Override
+				public FileVisitResult visitFile(java.nio.file.Path path, BasicFileAttributes attrs) throws IOException
+				{
+					File f = path.toFile();
+					if (f.isFile() && f.getName().toLowerCase().equals("pom.properties"))
+					{
+						Properties p = new Properties();
+						p.load(new FileReader(f));
+
+						isaacDbDependency =
+								new RestDependencyInfo(
+										p.getProperty("project.groupId"),
+										p.getProperty("project.artifactId"),
+										p.getProperty("project.version"),
+										p.getProperty("resultArtifactClassifier"),
+										p.getProperty("chronicles.type"));
+						
+						metadataVersion.set(p.getProperty("isaac-metadata.version"));
+						readDbMetadataFromProperties.set(true);
+						return readDbMetadataFromPom.get() ? FileVisitResult.TERMINATE : FileVisitResult.CONTINUE;
+					} else if (f.isFile() && f.getName().toLowerCase().equals("pom.xml")) {
+						DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
+						DocumentBuilder builder;
+						Document dDoc = null;
+						XPath xPath = XPathFactory.newInstance().newXPath();
+
+						try {
+							builder = domFactory.newDocumentBuilder();
+
+							dDoc = builder.parse(f);
+							
+							{
+								NodeList dbLicensesNodes = ((NodeList) xPath.evaluate("/project/licenses/license/name", dDoc, XPathConstants.NODESET));
+
+								log_.debug("Found {} license names in DB pom.xml", dbLicensesNodes.getLength());
+								for (int i = 0; i < dbLicensesNodes.getLength(); i++) {
+									Node currentLicenseNameNode = dbLicensesNodes.item(i);
+									String name = currentLicenseNameNode.getTextContent();
+
+									RestLicenseInfo license =
+											new RestLicenseInfo(
+													name,
+													((Node)xPath.evaluate("/project/licenses/license[name='" + name + "']/url", dDoc, XPathConstants.NODE)).getTextContent(),
+													((Node)xPath.evaluate("/project/licenses/license[name='" + name + "']/comments", dDoc, XPathConstants.NODE)).getTextContent());
+									dbLicenses.add(license);
+
+									log_.debug("Extracted license \"{}\" from DB pom.xml: {}", name, license.toString());
+								}
+							}
+							
+							{
+								NodeList dbDependenciesNodes = ((NodeList) xPath.evaluate("/project/dependencies/dependency/artifactId", dDoc, XPathConstants.NODESET));
+
+								log_.debug("Found {} dependency artifactIds in DB pom.xml", dbDependenciesNodes.getLength());
+								for (int i = 0; i < dbDependenciesNodes.getLength(); i++) {
+									Node currentDbDependencyArtifactIdNode = dbDependenciesNodes.item(i);
+									
+									String artifactId = currentDbDependencyArtifactIdNode.getTextContent();
+									String groupId = ((Node)xPath.evaluate("/project/dependencies/dependency[artifactId='" + artifactId + "']/groupId", dDoc, XPathConstants.NODE)).getTextContent();
+									String version = ((Node)xPath.evaluate("/project/dependencies/dependency[artifactId='" + artifactId + "']/version", dDoc, XPathConstants.NODE)).getTextContent();
+						
+									String classifier = null;
+									try {
+										classifier = ((Node)xPath.evaluate("/project/dependencies/dependency[artifactId='" + artifactId + "']/classifier", dDoc, XPathConstants.NODE)).getTextContent();
+									} catch (Throwable t) {
+										log_.debug("Problem reading \"classifier\" element for {}", artifactId);
+									}
+									String type = null;
+									try {
+										type = ((Node)xPath.evaluate("/project/dependencies/dependency[artifactId='" + artifactId + "']/type", dDoc, XPathConstants.NODE)).getTextContent();
+									} catch (Throwable t) {
+										log_.debug("Problem reading \"type\" element for {}", artifactId);
+									}
+
+									RestDependencyInfo dependencyInfo = new RestDependencyInfo(groupId, artifactId, version, classifier, type);
+									dbDependencies.add(dependencyInfo);
+
+									log_.debug("Extracted dependency \"{}\" from DB pom.xml: {}", artifactId, dependencyInfo.toString());
+								}
+							}
+						} catch (XPathExpressionException | SAXException | ParserConfigurationException e) {
+							throw new IOException(e);
+						}
+
+						readDbMetadataFromPom.set(true);
+						return readDbMetadataFromProperties.get() ? FileVisitResult.TERMINATE : FileVisitResult.CONTINUE;
+					}
+
+					return FileVisitResult.CONTINUE;
+				}
+			});
+		}
+
+		if (!readDbMetadataFromProperties.get())
+		{
+			log_.warn("Failed to read the metadata about the database from the database package.");
+		}
+		else
+		{
+			//Due to a quirk in how the DB poms are set up, we need to fill in this property
+			for (RestDependencyInfo dependency : dbDependencies)
+			{
+				if (dependency.version != null && "${isaac-metadata.version}".equals(dependency.version))
+				{
+					dependency.version = metadataVersion.get();
+					break;
+				}
+			}
+			log_.debug("Successfully read db properties from maven config files.  dbGroupId: {} dbArtifactId: {} dbVersion: {} dbClassifier: {} dbType: {}", 
+					isaacDbDependency.groupId, isaacDbDependency.artifactId, isaacDbDependency.version, isaacDbDependency.classifier, isaacDbDependency.type);
+		}
+	}
+	
+	private void loadIsaacMetdata() throws ParserConfigurationException, SAXException, IOException, DOMException, XPathExpressionException {
+		//read the ISAAC metadata
+		AtomicBoolean readIsaacAppMetadata = new AtomicBoolean(false);
+
+		//if running from eclipse - our launch folder should be "ISAAC-rest".
+		File f = new File("").getAbsoluteFile();
+		if (f.getName().endsWith("ISAAC-rest"))
+		{
+			File pom = new File(f, "pom.xml");
+			if (pom.isFile())
+			{
+				DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
+				DocumentBuilder builder = domFactory.newDocumentBuilder();
+				Document dDoc = builder.parse(pom);
+
+				XPath xPath = XPathFactory.newInstance().newXPath();
+				isaacVersion = ((Node) xPath.evaluate("/project/properties/isaac.version", dDoc, XPathConstants.NODE)).getTextContent();
+				scmUrl = ((Node) xPath.evaluate("/project/scm/url", dDoc, XPathConstants.NODE)).getTextContent();
+				apiImplementationVersion = ((Node) xPath.evaluate("/project/version", dDoc, XPathConstants.NODE)).getTextContent();
+				
+				NodeList appLicensesNodes = ((NodeList) xPath.evaluate("/project/licenses/license/name", dDoc, XPathConstants.NODESET));
+
+				log_.debug("Found {} license names", appLicensesNodes.getLength());
+				for (int i = 0; i < appLicensesNodes.getLength(); i++) {
+					Node currentLicenseNameNode = appLicensesNodes.item(i);
+					String name = currentLicenseNameNode.getTextContent();
+					
+					RestLicenseInfo appLicenseInfo =
+							new RestLicenseInfo(
+									name,
+									((Node)xPath.evaluate("/project/licenses/license[name='" + name + "']/url", dDoc, XPathConstants.NODE)).getTextContent(),
+									((Node)xPath.evaluate("/project/licenses/license[name='" + name + "']/comments", dDoc, XPathConstants.NODE)).getTextContent());
+					appLicenses.add(appLicenseInfo);
+					
+					log_.debug("Extracted license \"{}\" from ISAAC pom.xml: {}", name, appLicenseInfo.toString());
+				}
+
+				readIsaacAppMetadata.set(true);
+			}
+		}
+		//otherwise, running from an installation - we should have a META-INF folder
+		File appMetadata = new File("META-INF");
+		if (appMetadata.isDirectory())
+		{
+			Files.walkFileTree(appMetadata.toPath(), new SimpleFileVisitor<java.nio.file.Path>()
+			{
+				/**
+				 * @see java.nio.file.SimpleFileVisitor#visitFile(java.lang.Object, java.nio.file.attribute.BasicFileAttributes)
+				 */
+				@Override
+				public FileVisitResult visitFile(java.nio.file.Path path, BasicFileAttributes attrs) throws IOException
+				{
+					File visitFile = path.toFile();
+					if (visitFile.isFile() && visitFile.getName().toLowerCase().equals("pom.properties"))
+					{
+						Properties p = new Properties();
+						p.load(new FileReader(visitFile));
+
+						scmUrl = p.getProperty("scm.url");
+						isaacVersion = p.getProperty("isaac.version");
+						apiImplementationVersion = p.getProperty("project.version");
+						readIsaacAppMetadata.set(true);
+						return FileVisitResult.TERMINATE;
+					}
+					return FileVisitResult.CONTINUE;
+				}
+				
+			});
+		}
+		
+		if (!readIsaacAppMetadata.get())
+		{
+			log_.warn("Failed to read the metadata about the ISAAC-rest instance");
+		}
+		else
+		{
+			log_.debug("Successfully read ISAAC properties from maven config files.  API implementation version: {} scmUrl: {} isaacVersion: {}",
+					apiImplementationVersion, scmUrl, isaacVersion);
+		}
 	}
 }
