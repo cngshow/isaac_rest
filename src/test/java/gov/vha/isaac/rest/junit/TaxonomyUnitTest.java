66db6339c39b7777e09eefba875c0f064ce05f69
Vikas Sharma <vikas.sharma@vetsez.com>
2016-06-03 07:43:19 -0700

    added a mechanism to run the load test through mvn, to run the load test use, 'mvn -Pload-test exec:java'

--- src/test/java/gov/vha/isaac/rest/junit/TaxonomyUnitTest.java
+++ src/test/java/gov/vha/isaac/rest/junit/TaxonomyUnitTest.java
@@ -9,65 +9,46 @@ import java.util.Random;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriBuilder;
 
-import org.testng.annotations.Test;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
-import com.google.gson.Gson;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 
 import gov.vha.isaac.rest.api1.RestPaths;
-import gov.vha.isaac.rest.api1.data.concept.RestConceptVersion;
 
 /**
  * @author vikas.sharma
  *
  */
 public class TaxonomyUnitTest  extends SSLWSRestClientHelper {
-	public static final String BASE_URI = "http://localhost:8080/isaac-rest/rest/"+RestPaths.taxonomyPathComponent + RestPaths.versionComponent;
+
+	public static void main(String[] args) throws Exception
+	{
+		TaxonomyUnitTest taxonomyUnitTest = new TaxonomyUnitTest();
+		taxonomyUnitTest.testGetConceptVersionTaxonomy(args[0]);
+	}
 	
-	//@Test
-	public void testGetConceptVersionTaxonomy() {
-		//final String url ="http://localhost:8080/isaac-rest/rest/1/taxonomy/version";
-		//RestPaths.taxonomyPathComponent + RestPaths.versionComponent;
-		RestConceptVersion response = null;
+	@Parameters(value="baseURI")
+	public void testGetConceptVersionTaxonomy(@Optional("http://localhost:8080/isaac-rest/rest/") String baseURI) {
+		final String url = baseURI + RestPaths.taxonomyPathComponent + RestPaths.versionComponent;
 		Random randomGenerator = new Random();
-		 for (int idx = 1; idx <= 30; ++idx){
+		WebResource wr = getWebResource(url);
+		UriBuilder builder = UriBuilder.fromPath(url);
+		URI uri = builder.build();
+		System.out.println(uri);
+		for (int idx = 1; idx <= 30; ++idx){
 		try {
-			WebResource wr = getWebResource(BASE_URI);
-			UriBuilder builder = UriBuilder.fromPath(BASE_URI);
-			URI uri = builder.build();
-			System.out.println(uri);
 			ClientResponse clientResponse = wr.uri(uri).queryParam("childDepth", ""+randomGenerator.nextInt(5)).queryParam("countChildren", "true").queryParam("countChildren", "true")
 					.queryParam("expand", "chronology").queryParam("id",""+randomGenerator.nextInt(50000))
 					.queryParam("parentHeight","1").queryParam("stated","true").type(MediaType.APPLICATION_JSON)
 					.accept(MediaType.APPLICATION_JSON)
 					.get(ClientResponse.class);
-			String jsonResponse = clientResponse.getEntity(String.class);
-			System.out.println("Got response status:" + clientResponse.getStatus()
-			+ ", with jsonResponse:" + jsonResponse);
-			if (clientResponse.getClientResponseStatus() == ClientResponse.Status.OK) {
-				response = new Gson().fromJson(jsonResponse, RestConceptVersion.class);
-				System.out.println("response:"+response.toString());
-				if (response != null) {
-					System.out
-							.println("==========================================================");
-/*					for (String key : response.keySet()) {
-						System.out.println(key);
-						System.out.println(response.get(key));
-						System.out
-								.println("==========================================================");
-					}*/
-				}
-			} else {
-			}		
-				 
+			System.out.println(uri);
+			System.out.println("Got response status:" + clientResponse.getStatus()	+ ", with jsonResponse:" + clientResponse.getEntity(String.class));		
 				//checkFail(clientResponse);
-				System.out.println("------------------Taxonomy------------------------");
-				System.out.println(clientResponse.getEntity(String.class));
-				System.out.println("-------------------------------------------");
-			
 		} catch (Exception e) {
-			System.out.println(e);
+			System.out.println(e.getMessage());
 		}
 		 }
 	}
