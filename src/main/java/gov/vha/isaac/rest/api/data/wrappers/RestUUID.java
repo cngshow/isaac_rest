73b8a24310dedcaa7a41255f7cf16c5630a26076
Joel Kniaz <joel.kniaz@gmail.com>
2016-10-14 09:58:52 -0400

    Adding @XmlAccessorType(XmlAccessType.NONE)  to RestUUID

--- src/main/java/gov/vha/isaac/rest/api/data/wrappers/RestUUID.java
+++ src/main/java/gov/vha/isaac/rest/api/data/wrappers/RestUUID.java
@@ -20,6 +20,8 @@ package gov.vha.isaac.rest.api.data.wrappers;
 
 import java.util.UUID;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
@@ -36,6 +38,7 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
  * @author <a href="mailto:joel.kniaz.list@gmail.com">Joel Kniaz</a>
  */
 @XmlRootElement
+@XmlAccessorType(XmlAccessType.NONE)
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY, getterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE)
 @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY)
 public class RestUUID
