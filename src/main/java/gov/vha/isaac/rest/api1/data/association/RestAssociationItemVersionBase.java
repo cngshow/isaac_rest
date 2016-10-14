bee14e8987f5ea84cdf9a5ff51aaff9db29df814
darmbrust <daniel.armbrust.list@gmail.com>
2016-10-14 17:31:36 -0500

    add missing defaults

--- src/main/java/gov/vha/isaac/rest/api1/data/association/RestAssociationItemVersionBase.java
+++ src/main/java/gov/vha/isaac/rest/api1/data/association/RestAssociationItemVersionBase.java
@@ -36,7 +36,7 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.NONE)
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY, getterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE)
-@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY)
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, defaultImpl=RestAssociationItemVersionBase.class)
 public class RestAssociationItemVersionBase
 {
 	/**
