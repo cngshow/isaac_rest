a5e6ab76b7f2b146922bf7e2b4be89651a6e8d90
cshupp <cshupp@gmail.com>
2016-11-03 18:06:56 -0400

    Trying cookie first

--- src/main/java/gov/vha/isaac/rest/api1/export/ExportAPIs.java
+++ src/main/java/gov/vha/isaac/rest/api1/export/ExportAPIs.java
@@ -129,6 +129,6 @@ public class ExportAPIs
 		//https://github.com/johnculviner/jquery.fileDownload
 		//We must set the cookie fileDownload, this enables the GUI to provide feedback to the user telling them the file download
 		//was a success or a failure.
-		return Response.ok(stream).header("content-disposition", "attachment; filename = export.xml").cookie(new NewCookie("fileDownload", "true")).build();
+		return Response.ok(stream).cookie(new NewCookie("fileDownload", "true")).header("content-disposition", "attachment; filename = export.xml").build();
 	}
 }
