031b61e594ead5bf3340a4625d10b31a37c89a91
darmbrust <daniel.armbrust.list@gmail.com>
2016-12-02 20:00:57 -0600

    add a search by internal id function

--- src/main/java/gov/vha/isaac/rest/api1/data/RestSystemInfo.java
+++ src/main/java/gov/vha/isaac/rest/api1/data/RestSystemInfo.java
@@ -89,7 +89,7 @@ public class RestSystemInfo
 	 * data structure. 
 	 */
 	@XmlElement
-	String[] supportedAPIVersions = new String[] {"1.9.3"};
+	String[] supportedAPIVersions = new String[] {"1.9.4"};
 	
 	/**
 	 * REST API Implementation Version - aka the version number of the software running here.
