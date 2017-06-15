4da1d50a2115e514cb94f423cb12e24ecdfd977e
Joel Kniaz <joel.kniaz@gmail.com>
2017-06-15 15:53:22 -0400

    Removing useless token manipulation code

--- src/main/java/gov/vha/isaac/rest/api1/coordinate/CoordinateAPIs.java
+++ src/main/java/gov/vha/isaac/rest/api1/coordinate/CoordinateAPIs.java
@@ -348,11 +348,6 @@ public class CoordinateAPIs
 	{
 		// Log value of ssoToken parameter
 		//log.info(RequestParameters.ssoToken + "==\"" + ssoToken + "\"");
-		// TODO figure out why we need to use RequestInfo.get().getParameters().get(RequestParameters.ssoToken) instead of ssoToken
-		if (RequestInfo.get().getParameters().get(RequestParameters.ssoToken) != null && RequestInfo.get().getParameters().get(RequestParameters.ssoToken).size() > 0) {
-			ssoToken = RequestInfo.get().getParameters().get(RequestParameters.ssoToken).iterator().next();
-		}
-		//log.info(RequestParameters.ssoToken + " in RequestInfo==\"" + ssoToken + "\"");
 
 		SecurityUtils.validateRole(securityContext, getClass());
 		RequestInfoUtils.validateIncompatibleParameters(RequestInfo.get().getParameters(), RequestParameters.editToken, RequestParameters.ssoToken, RequestParameters.userId);
